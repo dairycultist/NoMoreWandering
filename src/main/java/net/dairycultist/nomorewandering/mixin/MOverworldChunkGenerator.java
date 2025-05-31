958621fbcc8024f7b315356fbccf7e85ad3bed59
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-31 18:09:49 -0400

    generate copper ore

--- src/main/java/net/dairycultist/nomorewandering/mixin/MOverworldChunkGenerator.java
+++ src/main/java/net/dairycultist/nomorewandering/mixin/MOverworldChunkGenerator.java
@@ -1,5 +1,6 @@
 package net.dairycultist.nomorewandering.mixin;
 
+import net.dairycultist.nomorewandering.NoMoreWandering;
 import net.minecraft.block.Block;
 import net.minecraft.util.math.noise.OctavePerlinNoiseSampler;
 import net.minecraft.world.World;
@@ -7,6 +8,7 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkSource;
 import net.minecraft.world.gen.chunk.OverworldChunkGenerator;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -56,18 +58,30 @@ public class MOverworldChunkGenerator {
     @Inject(method = "decorate", at = @At(value = "TAIL"))
     public void decorate(ChunkSource source, int chunkX, int chunkZ, CallbackInfo ci) {
 
+        // trees
         int chunkBlockX = chunkX * 16;
         int chunkBlockZ = chunkZ * 16;
 
         Biome biome = this.world.method_1781().getBiome(chunkBlockX + 16, chunkBlockZ + 16);
 
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
+
             int x = chunkBlockX + this.random.nextInt(16) + 8;
             int z = chunkBlockZ + this.random.nextInt(16) + 8;
 
             Feature tree = biome.getRandomTreeFeature(this.random);
             tree.prepare(1.0, 1.0, 1.0);
             tree.generate(this.world, this.random, x, this.world.getTopY(x, z), z);
+        }
+
+        // copper ore
+        for(int i = 0; i < 16; i++) {
+
+            new OreFeature(NoMoreWandering.COPPER_ORE.id, 7).generate(this.world, this.random,
+                    chunkBlockX + this.random.nextInt(16),
+                    this.random.nextInt(24) + 48,
+                    chunkBlockZ + this.random.nextInt(16)
+            );
         }
     }
 
